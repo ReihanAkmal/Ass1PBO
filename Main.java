efd11b896fa7fff2c10320774b50fb61df3f29df
ReihanAkmal <92454237+ReihanAkmal@users.noreply.github.com>
2021-10-28 00:20:56 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+            //Asuransi dan Dropship
+            pengirimanbarang barang1 = new pengirimanbarang(1, "N0001", "JNE", 21000, "Dropship1", "Surabaya", "Bejo", "DayeuhKolot", true, true, 40000);
+
+            //Asuransi
+            pengirimanbarang barang2 = new pengirimanbarang(2, "N0003", "J&T", 21000, "Eiger", "Bandung Kopo", "Tini", "DayeuhKolot", true, 40000);
+
+            //Normal
+            pengirimanbarang barang3 = new pengirimanbarang(3, "N0004", "Pos Indonesia", 23000, "Eiger", "Bandung Kopo", "Bambang", "DayeuhKolot");
+
+            //Dropship
+            pengirimanbarang barang4 = new pengirimanbarang(4, "N0002", "Si Cepat", 24000, "Dropship2", "Pontianak", "Joko", "DayeuhKolot", true);
+        }
+    }
+
